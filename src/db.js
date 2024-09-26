1a03ec83932594957e0be23dbcd2f83f60ff8a9f
cuongnm <cuong2th2@gmail.com>
2024-09-26 11:25:00 +0700

    add db.js

---
+++ src/db.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    source: '../converted/Snowdon Towers Sample Electrical(2x3CV20).xkt',
+    output: '../ifc/Electrical.ifc',
+  },
+  {
+    source: '../converted/Snowdon Towers Sample HVAC(2x3CV20).xkt',
+    output: '../ifc/HVAC.ifc',
+  },
+  {
+    source: '../converted/Snowdon Towers Sample Plumbing (2x3CV20).xkt',
+    output: '../ifc/Plumbing.ifc',
+  },
+  {
+    source: '../converted/Snowdon Towers Sample Structural(2x3CV20).xkt',
+    output: '../ifc/Structural.ifc',
+  },
+];
